648af11b60ab2d51c666ea160aba559ce623a32d
Megumi Boogh <megumi.boogh92@gmail.com>
2026-04-30 16:16:33 +0200

    Test tempsensor stub

--- main/source/main.cpp
+++ main/source/main.cpp
@@ -8,6 +8,9 @@
 #include "driver/gpio/stub.h"
 #include "driver/gpio/esp32s3.h"
 
+#include "driver/tempsensor/stub.h" //----------------------
+
+#include "esp_log.h"
 
 extern "C" void app_main()
 {
@@ -15,10 +18,17 @@ extern "C" void app_main()
 
     esp32s3.write(true);
     esp32s3.read();
+    
     while(1)
     {
     esp32s3.toggle();
-    // Vänta i 500 millisekunder
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-}
+        // Vänta i 500 millisekunder
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+        driver::tempsensor::Stub tmp1;  //------------------------------
+        tmp1.simulateInitialized(true);
+        tmp1.simulateTemperature(17);
+
+        ESP_LOGI("main", "Temperature: %d\n", tmp1.read());
+    }
 }

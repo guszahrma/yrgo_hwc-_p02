ae8ee70a11ff5bab8d2a191dc3418bf928022443
Megumi Boogh <megumi.boogh92@gmail.com>
2026-05-05 17:04:46 +0200

    Add tmp36

---
+++ main/include/driver/tempsensor/tmp36.h
@@ -0,0 +1,60 @@
+/**
+ * @brief Temperature sensor.
+ */
+#pragma once
+
+#include <cstdint>
+
+#include "driver/tempsensor/interface.h"
+
+namespace driver{
+    namespace adc{class Interface;} 
+}
+
+namespace driver::tempsensor
+{
+/**
+ * @brief Tmp36
+ */
+class Tmp36 final : public Interface
+{
+public:
+    /**
+     * @brief constructor
+     * 
+     * @param[in] initialTemp Initial temperature (default = 0).
+     */
+    explicit Tmp36(adc::Interface& adc, std::uint8_t pin) noexcept;
+
+    /**
+     * @brief Destructor.
+     */
+    ~Tmp36() noexcept override;
+
+    /**
+     * @brief Check if the temperature sensor is initialized.
+     * 
+     * @return True if initialized, false otherwise.
+     */
+    bool isInitialized() const noexcept override;
+
+    /**
+     * @brief Read temperature.
+     * 
+     * @return Temperature in degrees Celsius.
+     */
+    std::int16_t read() const noexcept override;
+
+    Tmp() = delete;
+    Tmp36(const Tmp36&) = delete;
+    Tmp36(Tmp36&&) = delete;
+    Tmp36& operator=(const Tmp26&) = delete;
+    Tmp36& operator=(Tmp&&) = delete;
+
+private:
+    adc::Interface& myAdc;
+    const std::uint8_t myPin;
+    bool myInitialized;
+
+};
+} // namespace driver::tempsensor
---
+++ main/source/driver/tempsensor/tmp36.cpp
@@ -0,0 +1,54 @@
+/**
+ * @brief Temperature sensor.
+ */
+#include <cstdint>
+#include <cstdio>
+
+#include "driver/tempsensor/interface.h"
+#include "driver/tempsensor/tmp36.h"
+
+
+namespace driver::tempsensor
+{
+    /**
+     * @brief constructor
+     * 
+     * @param[in] initialTemp Initial temperature (default = 0).
+     */
+    Tmp36::Tmp36(adc::Interface& adc, const uint8_t pin) noexcept
+        : myAdc{adc}  //värde från adc(0-1023)
+        , myPin{pin}
+        , myInitialized{true} //tillstånd av sensor
+    {}
+
+    /**
+     * @brief Check if the temperature sensor is initialized.
+     * 
+     * @return True if initialized, false otherwise.
+     */
+    bool Tmp36::isInitialized() const noexcept {
+        return myInitialized;
+    }
+
+    /**
+     * @brief Read temperature.
+     * 
+     * @return Temperature in degrees Celsius.
+     */
+    std::int16_t Tmp36::read() const noexcept {
+        if(!isInitialized()){return 0;}
+
+        #if 0
+        // Beräkna inspänning i V från tempsensorn.
+        const double inputVoltage{myAdc.readVoltage(myPin)};
+        
+        // Omvandla inspänning till temperatur med formeln
+        // T  = 100 * Uin - 50.
+        return 100.0 * inputVoltage - 50.0;
+        #else
+            // Just nu skickar vi tillbaka 0, då vi ej har en färdig ADC.
+            return 0;
+        #endif
+    }
+
+} // namespace driver::tempsensor
